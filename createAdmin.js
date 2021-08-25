345277b5060c279b3eae8d0c38ca5e08c81bcce0
jalips <jservice@protonmail.com>
2021-08-25 15:23:50 +0200

    Add script to create user and command to connect

---
+++ createAdmin.js
@@ -0,0 +1,19 @@
+
+// mongod.conf
+//   authorization: disabled
+
+var db = db.getSiblingDB("admin"); // use admin
+
+if(db.getUser("admin")){
+    db.dropAllUsers();
+}
+db.createUser({
+    user: "admin",
+    pwd: "password",
+    roles: [{
+        role: "userAdminAnyDatabase", 
+        db:"admin"
+    }]
+});
+
+
