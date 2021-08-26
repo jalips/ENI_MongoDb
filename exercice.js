04aa88afe08e60f6d8968c06e0bae975bb494647
jalips <jservice@protonmail.com>
2021-08-26 11:13:48 +0200

    Exo 1 OK

--- exercice.js
+++ exercice.js
@@ -1,13 +1,13 @@
-var exo_number = 0;
+var exo_number = 2;
 
 db = db.getSiblingDB("admin"); 
 db.auth("admin","password"); 
-db = db.getSiblingDB("adherentdb");
 
 
 switch(exo_number){
     case 0:
         print("Exo 0 - Exemple");
+        db = db.getSiblingDB("adherentdb");
         var animateurs = db.animateurs.find({}, {nom:true, prenom:true});
         while(animateurs.hasNext()){
             printjson(animateurs.next());
@@ -15,12 +15,23 @@ switch(exo_number){
         break;
     case 1:
         print("Exo 1");
-        var animateurs = db.animateurs.find({}, {nom:true, prenom:true});
-        while(animateurs.hasNext()){
-            printjson(animateurs.next());
+        db = db.getSiblingDB("adherentdb");
+        var adherents = db.adherents.find({ville:"NANTES"}, {nom:true, prenom:true, ville:true});
+        adherents.sort({nom:1});
+        print("nbAdherents: "+adherents.lenght);
+        while(adherents.hasNext()){
+            printjson(adherents.next());
         }
         break;
     case 2:
+        print("Exo 2");
+        db = db.getSiblingDB("adherentdb");
+        var adherents = db.adherents.find({ville:"NANTES"}, {nom:true, prenom:true, ville:true});
+        adherents.sort({nom:1});
+        print("nbAdherents: "+adherents.lenght);
+        while(adherents.hasNext()){
+            printjson(adherents.next());
+        }
 
         break;
         
